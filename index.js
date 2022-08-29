edc55266a0407ffe51d87c605e2b44b4be3fb4be
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-29 13:53:31 -0400

    commit tree 16ecc95c85dd8266408f04794b454aa965dd41c4

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let product;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+let max = Math.max(5, 10, 15, 20);
